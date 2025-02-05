29cc5ae61b50322b9509f60d37c1aaef81016dc7
Cristina S. Montana <cristina.s.montana@gmail.com>
2025-02-04 18:57:24 -0800

    Initial commit - MyCabBuddy Microservices

---
+++ src/main/java/com/mycab/buddy/MyCabBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.mycab.buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCabBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCabBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycab/buddy/MyCabBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycab.buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCabBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
