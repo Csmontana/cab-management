029b17b4340297a7960191a4c2ac9d40cf2a79ec
Cristina S. Montana <cristina.s.montana@gmail.com>
2025-02-04 19:13:40 -0800

    Finalized microservices structure

---
+++ eureka-server/src/main/java/com/mycab/buddy/eureka/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.mycab.buddy.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer  // This annotation is necessary to enable Eureka Server
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
